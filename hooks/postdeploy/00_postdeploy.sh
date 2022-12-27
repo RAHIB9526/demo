22293f5fe34529f0c486da695b60b53fa7b4e2ae
Mohammed Rahib <muhammedrahib86@gmail.com>
2022-12-27 13:06:34 +0530

    added

---
+++ hooks/postdeploy/00_postdeploy.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+echo "----------------------------- post deploy script starting ---------------------------------"
+
+echo "----------------------------- post deploy script complete ---------------------------------"
---
+++ hooks/prebuild/00_prebuild.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+sudo yum install -y python3-devel mysql-devel
+echo "prebuild script complete"
+
